aa787a44deb64a138af16d41d11cdaa40b392a8e
chr <3537119133@qq.com>
2024-12-22 14:17:14 +0800

    Site updated: 2024-12-22 14:16:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/14/Hello-Everyone/","2024/12/09/hello-world/","2024/12/22/111/"];function toRandomPost(){
+var posts=["2024/12/22/111/","2024/12/14/Hello-Everyone/","2024/12/09/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
