86274087a1179083d1341cc0078f209538c60849
chr <3537119133@qq.com>
2024-12-25 17:42:47 +0800

    Site updated: 2024-12-25 17:42:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/22/111/","2024/12/14/Hello-Everyone/","2024/12/25/使用VScode和EIDE插件开发STM32/","2024/12/09/hello-world/"];function toRandomPost(){
+var posts=["2024/12/09/hello-world/","2024/12/25/使用VScode和EIDE插件开发STM32/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
